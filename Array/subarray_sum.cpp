dbecd48dc3e638a530c7639265cb6b8c82228a33
kartik800 <54595695+kartik800@users.noreply.github.com>
2021-05-15 20:18:09 +0530

    Added Array practice question

---
+++ Array/subarray_sum.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//ques: Given a unsorted array A of size N of non-negative integers, find a continuous subarray which adds to a given number S.
+// Input N= 5,S = 2
+// A[] = {1,2,3,7,5}
+// Output : 2 4
+
+int main()
+{
+    int n, s;
+    cin >> n >> s;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int i = 0, j = 0, st = -1, en = -1, sum = 0;
+
+    while (j < n && sum + arr[j] <= s)
+    {
+        sum += arr[j];
+        j++;
+    }
+    if (sum == s)
+    {
+        cout << i + 1 << " " << j << endl;
+        return 0;
+    }
+    while (j < n)
+    {
+        sum += arr[j];
+        while (sum > s)
+        {
+            sum -= arr[i];
+            i++;
+        }
+        if (sum == s)
+        {
+            st = i + 1;
+            en = j + 1;
+        }
+        j++;
+    }
+    cout << st << " " << en << endl;
+    return 0;
+}
